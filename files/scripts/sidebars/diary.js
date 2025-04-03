ce9709750b7341fb89a6c0d3c18131ada7d0ad15
angelnetcast <angelnetcast@protonmail.com>
2025-04-03 15:53:20 -0500

    accessibility update for diaries

---
+++ files/scripts/sidebars/diary.js
@@ -0,0 +1,28 @@
+const sidebar = ` 
+<h1>navigation</h1>
+
+				<h2>2025</h2>
+				<ul>
+					<li>
+						<a href="/pages/diaries/25/01jan.html" >january</a>
+					</li>
+					<li>
+						<a href="/pages/diaries/25/02feb.html" >february</a>
+					</li>
+					<li>
+						<a href="/pages/diaries/25/03mar.html" >march</a>
+					</li>
+				</ul>
+
+				<h2>2024</h2>
+				<ul>
+					<li><a href="/pages/diaries/24/08aug.html" >august</a></li>
+					<li><a href="/pages/diaries/24/09sep.html" >september</a></li>
+					<li><a href="/pages/diaries/24/10oct.html" >october</a></li>
+					<li><a href="/pages/diaries/24/11nov.html" >november</a></li>
+					<li><a href="/pages/diaries/24/12dec.html" >december</a></li>
+				</ul>
+				
+`;
+
+document.getElementById("sidebar").innerHTML = sidebar;
