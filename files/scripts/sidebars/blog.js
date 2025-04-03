274e811fa071ddff2b8516466d2a84d9dd9a42e6
angelnetcast <angelnetcast@protonmail.com>
2025-04-03 12:55:43 -0500

    accessibility update for blogs

---
+++ files/scripts/sidebars/blog.js
@@ -0,0 +1,101 @@
+const sidebar = ` 
+<h2>opinion writing</h2>
+				<div class="entry">
+					<div class="ehead">
+						<h3>
+							<a href="/pages/blogs/ns2direct.html" 
+								>my thoughts on the nintendo switch 2</a
+							>
+						</h3>
+					</div>
+					<div class="edesc">
+						<p>a review of the nintendo direct and other console information</p>
+					</div>
+				</div>
+
+				<h2>personal entries</h2>
+				<div class="entry">
+					<div class="ehead">
+						<h3>
+							<a href="/pages/blogs/sonicsymphony.html" 
+								>sonic symphony</a
+							>
+						</h3>
+					</div>
+					<div class="edesc">
+						<p>my "technically" first concert!</p>
+					</div>
+				</div>
+				<h2>projects</h2>
+				<div class="entry">
+					<div class="ehead">
+						<h3>
+							<a href="/pages/blogs/pocketpc.html" 
+								>finding a use for the pocket pc 2004</a
+							>
+						</h3>
+					</div>
+					<div class="edesc">
+						<p>
+							a collection of (hopefully) multiple entries of me modifying old shit in
+							my garage
+						</p>
+					</div>
+				</div>
+				<div class="entry">
+					<div class="ehead">
+						<h3>
+							<a href="/pages/blogs/vmsetup.html" 
+								>&#9734; NEW - my virtural machines (and other old stuff)</a
+							>
+						</h3>
+					</div>
+					<div class="edesc">
+						<p>
+							yapping about my virtural machines and other retro things i have set up
+							for funsies.
+						</p>
+					</div>
+				</div>
+				<h2>tutorials</h2>
+				<div class="entry">
+					<div class="ehead">
+						<h3>
+							<a href="/pages/blogs/miis.html" 
+								>make miis on your pc</a
+							>
+						</h3>
+					</div>
+					<div class="edesc">
+						<p>how to make miis on your computer (and make them spin)</p>
+					</div>
+				</div>
+				<div class="entry">
+					<div class="ehead">
+						<h3>
+							<a href="/pages/blogs/3dsmusic.html" 
+								>music from 3ds games</a
+							>
+						</h3>
+					</div>
+					<div class="edesc">
+						<p>how i get music from my 3ds onto my computer</p>
+					</div>
+				</div>
+				<div class="entry">
+					<div class="ehead">
+						<h3>
+							<a href="/pages/blogs/pspvids.html" >.mp4 to psp</a>
+						</h3>
+					</div>
+					<div class="edesc">
+						<p>
+							how to make videos actually work on a psp (yeah drag and drop doesn't
+							just work, tragic)
+						</p>
+					</div>
+				</div>
+				
+`;
+
+document.getElementById("sidebar").innerHTML = sidebar;
