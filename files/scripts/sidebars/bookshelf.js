059b3d3b1b6686c22e181934a3db5ce7c7b46c49
angelnetcast <angelnetcast@protonmail.com>
2025-08-18 01:24:55 -0500

    yuri overtaking

--- files/scripts/sidebars/bookshelf.js
+++ files/scripts/sidebars/bookshelf.js
@@ -1,6 +1,8 @@
 const sidebar = ` 
 	<h1>shelves</h1>
 			<ul>
+				<li><a href="/pages/bookshelf.html">bookshelf</a></li>
+				<li><a href="/pages/library/manga.html">manga</a></li>
 				<li><a href="/pages/home.html">home?</a></li>
 			</ul>
 				
